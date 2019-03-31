96e2be319ad402b0b02f28804c3fce129fced736
CylonicRaider <CylonicRaider@users.noreply.github.com>
2019-04-01 01:07:24 +0200

    Only maintain a WebSocket while chatting

--- ssrtt.py
+++ ssrtt.py
@@ -17,6 +17,7 @@ class Stream:
         self.code = code
         self.data = data
         self.locked = False
+        self.onevent = None
         self.cond = threading.Condition()
     def __enter__(self):
         return self.cond.__enter__()
@@ -25,7 +26,7 @@ class Stream:
     def __iter__(self):
         return iter(self.iter())
     def __call__(self, data):
-        self.post(data)
+        self.update(data)
     def iter(self, timeout=None):
         while 1:
             with self:
@@ -36,15 +37,25 @@ class Stream:
             if self.locked: return False
             self.locked = True
             self.cond.notifyAll()
+            if self.onevent: self.onevent('lock')
             return True
     def unlock(self):
         with self:
             self.locked = False
             self.cond.notifyAll()
-    def post(self, data):
+            if self.onevent: self.onevent('unlock')
+    def update(self, data):
         with self:
             self.data = data
             self.cond.notifyAll()
+            if self.onevent: self.onevent('update', data)
+    def set_onevent(self, handler):
+        with self:
+            self.onevent = handler
+    def cas_onevent(self, check, handler):
+        with self:
+           if self.onevent is check:
+               self.onevent = handler
 
 def spawn_thread(func, *args, **kwds):
     thr = threading.Thread(target=func, args=args, kwargs=kwds)
@@ -88,13 +99,29 @@ class SSRTTRequestHandler(
             self.wfile.flush()
             old_data, old_locked = data, locked
 
-    def run_write(self, code):
+    def run_write(self, code, readcode=None):
+        def handle_event(name, data=None):
+            if name == 'lock':
+                conn.write_text_frame('s:active')
+            elif name == 'unlock':
+                conn.write_text_frame('s:hangup')
+            elif name == 'update':
+                conn.write_text_frame('t:' + data)
         stream = self.get_stream(code)
         if not stream.lock():
             self.send_code(409, 'Stream is already being written')
             return
+        readstream = None if readcode is None else self.get_stream(readcode)
         try:
             conn = self.handshake()
+            if readstream:
+                with readstream:
+                    readstream.set_onevent(handle_event)
+                    if readstream.locked:
+                        handle_event('lock')
+                    else:
+                        handle_event('unlock')
+                    handle_event('update', readstream.data)
             conn.write_text_frame('e:' + stream.data)
             while 1:
                 msg = conn.read_frame()
@@ -106,6 +133,7 @@ class SSRTTRequestHandler(
                 if cnt.startswith('T:'):
                     stream(cnt[2:])
         finally:
+            if readstream: readstream.cas_onevent(handle_event, None)
             stream.unlock()
 
     def do_GET(self):
@@ -153,6 +181,7 @@ class SSRTTRequestHandler(
                         static = 'static/' + parts[1] + '.html'
             elif len(parts) == 3:
                 code = parts[0] + '/' + parts[2]
+                flipcode = parts[2] + '/' + parts[0]
                 if path.endswith('/'):
                     # No directories here, too
                     self.send_redirect(301, '../' + parts[-1])
@@ -162,11 +191,7 @@ class SSRTTRequestHandler(
                     static = 'static/chat.html'
                 elif parts[1] == 'ws':
                     # Writing the chat stream
-                    self.run_write(code)
-                    return
-                elif parts[1] == 'get':
-                    # Reading the chat stream
-                    self.run_read(code)
+                    self.run_write(code, flipcode)
                     return
             self.send_cache(static and self.CACHE.get(static))
         except IOError:
